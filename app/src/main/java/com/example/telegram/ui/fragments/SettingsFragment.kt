e0f637657ccf427432e8777b5c8f58ed6297cbc0
alextsybin <alexander.tsyin@gmail.com>
2021-01-21 01:45:07 +0300

    Settings options menu

--- app/src/main/java/com/example/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/telegram/ui/fragments/SettingsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.telegram.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.fragment.app.Fragment
 import com.example.telegram.R
 
@@ -10,5 +12,11 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_options_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
